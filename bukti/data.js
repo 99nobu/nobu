e41827459a05de98ad7eb98ab7be305661b918a5
99nobu <agentnobu99@gmail.com>
2024-08-05 23:49:33 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -1,9 +1,9 @@
 const items = [
-    // Add your items here, each item should be an object with img, title, and table data
+
     {
         img: 'https://i.postimg.cc/wvdK9wxL/prag.webp',
-        title: 'Item 1',
-        table: [['Row 1 Col 1', 'Row 1 Col 2'], ['Row 2 Col 1', 'Row 2 Col 2'], ['Row 3 Col 1', 'Row 3 Col 2']]
+        title: 'Olympus 1000',
+        table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 25.000'], ['WD', 'Rp 2.500.000']]
     },
     {
         img: 'https://wikagrup.com/wp-content/uploads/2024/07/BUKTI-JEPE-1024x1024.png',
