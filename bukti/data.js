8b45f417234ab4c71d2306109d2d44e29133a239
99nobu <agentnobu99@gmail.com>
2024-08-06 12:20:03 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -6,27 +6,27 @@ const items = [
         table: [['Tanggal', '03-08-2024'], ['Modal', 'Rp 25.000'], ['WD', 'Rp 1.400.000']]
     },
     {
-        img: 'bukti/img/BUKTI JEPE (2).webp',
+        img: '/bukti/img/BUKTI JEPE (2).webp',
         title: 'Mochimon',
         table: [['Tanggal', '03-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '13.000.000']]
     },
     {
-        img: 'bukti/img/BUKTI JEPE .webp',
+        img: '/bukti/img/BUKTI JEPE .webp',
         title: 'Mahjong Ways',
         table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '8.000.000']]
     },
     {
-        img: 'bukti/img/BUKTI JEPE .webp',
+        img: '/bukti/img/BUKTI JEPE .webp',
         title: 'Lucky Neko',
         table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 50.000'], ['WD', '2.951.000']]
     },
     {
-        img: 'bukti/img/BUKTI JEPE (3).webp',
+        img: '/bukti/img/BUKTI JEPE (3).webp',
         title: 'Mahjong Ways',
         table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '8.000.000']]
     },
     {
-        img: 'bukti/img/BUKTI JEPE (4).webp',
+        img: '/bukti/img/BUKTI JEPE (4).webp',
         title: 'Mahjong Ways 2',
         table: [['Tanggal', '06-08-2024'], ['Modal', 'Rp 30.000'], ['WD', '1.000.000']]
     },
