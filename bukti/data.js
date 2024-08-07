07c6dc45f8c0156a5d98fd1e3fd89c632053e4f1
Dvdwinata <dvdwinata11@gmail.com>
2024-08-07 20:42:25 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -1,69 +1,9 @@
 const items = [
 
     {
-        img: '/bukti/img/tgl10bln1.png',
-        title: 'Olympus 1000',
-        table: [['Tanggal', '10-01-2024'], ['Modal', 'Rp 40.000'], ['WD', 'Rp 3.000.000']]
-    },
-    {
-        img: '/bukti/img/tgl7bln3.png',
-        title: 'Starlight Princess',
-        table: [['Tanggal', '07-03-2024'], ['Modal', 'Rp 50.000'], ['WD', 'Rp 3.000.000']]
-    },
-    {
-        img: '/bukti/img/tgl10bln3.png',
-        title: 'SINGAPORE',
-        table: [['Tanggal', '10-03-2024'], ['Modal', 'Rp 20.000'], ['WD', 'Rp 13.000.000']]
-    },
-    {
-        img: '/bukti/img/tgl11bln3.png',
-        title: 'Gates of Olympus',
-        table: [['Tanggal', '11-03-2024'], ['Modal', 'Rp 70.000'], ['WD', 'Rp 5.550.000']]
-    },
-    {
-        img: '/bukti/img/tgl19bln3.png',
-        title: 'Sugar Rush 1000',
-        table: [['Tanggal', '19-03-2024'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 7.500.000']]
-    },
-    {
-        img: '/bukti/img/tgl1bln4.png',
-        title: 'Gates of Olympus 1000',
-        table: [['Tanggal', '01-04-2024'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 5.950.000']]
-    },
-    {
-        img: '/bukti/img/tgl5bln5.png',
-        title: 'Starlight Princess 1000',
-        table: [['Tanggal', '05-05-2024'], ['Modal', 'Rp 89.000'], ['WD', 'Rp 4.600.000']]
-    },
-    {
-        img: '/bukti/img/tgl19bln6.png',
-        title: 'Gates of Olympus',
-        table: [['Tanggal', '19-06-2024'], ['Modal', 'Rp 50.000'], ['WD', 'Rp 3.950.000']]
-    },
-    {
-        img: '/bukti/img/BUKTI JEPE (5).webp',
-        title: 'Olympus 1000',
-        table: [['Tanggal', '03-08-2024'], ['Modal', 'Rp 25.000'], ['WD', 'Rp 1.400.000']]
-    },
-    {
-        img: '/bukti/img/BUKTI JEPE (2).webp',
-        title: 'Mochimon',
-        table: [['Tanggal', '03-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '13.000.000']]
-    },
-    {
-        img: '/bukti/img/tgl4bln8.png.png',
-        title: 'Taiwan',
-        table: [['Tanggal', '04-08-2024'], ['Modal', 'Rp 450.000'], ['WD', '10.400.000']]
-    },
-    {
-        img: '/bukti/img/BUKTI JEPE.webp',
-        title: 'Mahjong Ways',
-        table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '8.000.000']]
-    },
-    {
-        img: '/bukti/img/BUKTI JEPE (3).webp',
-        title: 'Lucky Neko',
-        table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 50.000'], ['WD', '2.951.000']]
+        img: '/bukti/img/tgl7bln8.png',
+        title: 'Sweet Bonanza 1000',
+        table: [['Tanggal', '07-08-2024'], ['Modal', 'Rp 120.000'], ['WD', '3.550.000']]
     },
     {
         img: '/bukti/img/BUKTI JEPE (4).webp',
@@ -71,9 +11,69 @@ const items = [
         table: [['Tanggal', '06-08-2024'], ['Modal', 'Rp 30.000'], ['WD', '1.000.000']]
     },
     {
-        img: '/bukti/img/tgl7bln8.png',
-        title: 'Sweet Bonanza 1000',
-        table: [['Tanggal', '07-08-2024'], ['Modal', 'Rp 120.000'], ['WD', '3.550.000']]
+        img: '/bukti/img/BUKTI JEPE (3).webp',
+        title: 'Lucky Neko',
+        table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 50.000'], ['WD', '2.951.000']]
+    },
+    {
+        img: '/bukti/img/BUKTI JEPE.webp',
+        title: 'Mahjong Ways',
+        table: [['Tanggal', '05-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '8.000.000']]
+    },
+    {
+        img: '/bukti/img/tgl4bln8.png.png',
+        title: 'Taiwan',
+        table: [['Tanggal', '04-08-2024'], ['Modal', 'Rp 450.000'], ['WD', '10.400.000']]
+    },
+    {
+        img: '/bukti/img/BUKTI JEPE (2).webp',
+        title: 'Mochimon',
+        table: [['Tanggal', '03-08-2024'], ['Modal', 'Rp 100.000'], ['WD', '13.000.000']]
+    },
+    {
+        img: '/bukti/img/BUKTI JEPE (5).webp',
+        title: 'Olympus 1000',
+        table: [['Tanggal', '03-08-2024'], ['Modal', 'Rp 25.000'], ['WD', 'Rp 1.400.000']]
+    },
+    {
+        img: '/bukti/img/tgl19bln6.png',
+        title: 'Gates of Olympus',
+        table: [['Tanggal', '19-06-2024'], ['Modal', 'Rp 50.000'], ['WD', 'Rp 3.950.000']]
+    },
+    {
+        img: '/bukti/img/tgl5bln5.png',
+        title: 'Starlight Princess 1000',
+        table: [['Tanggal', '05-05-2024'], ['Modal', 'Rp 89.000'], ['WD', 'Rp 4.600.000']]
+    },
+    {
+        img: '/bukti/img/tgl1bln4.png',
+        title: 'Gates of Olympus 1000',
+        table: [['Tanggal', '01-04-2024'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 5.950.000']]
+    },
+    {
+        img: '/bukti/img/tgl19bln3.png',
+        title: 'Sugar Rush 1000',
+        table: [['Tanggal', '19-03-2024'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 7.500.000']]
+    },
+    {
+        img: '/bukti/img/tgl11bln3.png',
+        title: 'Gates of Olympus',
+        table: [['Tanggal', '11-03-2024'], ['Modal', 'Rp 70.000'], ['WD', 'Rp 5.550.000']]
+    },
+    {
+        img: '/bukti/img/tgl10bln3.png',
+        title: 'SINGAPORE',
+        table: [['Tanggal', '10-03-2024'], ['Modal', 'Rp 20.000'], ['WD', 'Rp 13.000.000']]
+    },
+    {
+        img: '/bukti/img/tgl7bln3.png',
+        title: 'Starlight Princess',
+        table: [['Tanggal', '07-03-2024'], ['Modal', 'Rp 50.000'], ['WD', 'Rp 3.000.000']]
+    },
+    {
+        img: '/bukti/img/tgl10bln1.png',
+        title: 'Olympus 1000',
+        table: [['Tanggal', '10-01-2024'], ['Modal', 'Rp 40.000'], ['WD', 'Rp 3.000.000']]
     },
     
 ];
