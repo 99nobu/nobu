b94023c1cc9ddc1e33fa0d78ae404405a6637d53
99nobu <agentnobu99@gmail.com>
2024-10-15 01:17:24 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -1,6 +1,10 @@
 const items = [
 
     {
+        img: '/bukti/img/2024-10-15.webp',
+        title: 'Olympus 1000',
+        table: [['Tanggal', '14-10-2024'], ['Modal', 'Rp 20.000'], ['WD', 'Rp 3.000.000']]
+    },{
         img: '/bukti/img/2024-10-091N.webp',
         title: 'Starlight 1000 & 15000 Mania',
         table: [['Tanggal', '09-10-2024'], ['Modal', 'Rp 1.000.000'], ['WD', 'Rp 30.700.000']]
