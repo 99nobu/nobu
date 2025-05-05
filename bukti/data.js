786b56c3e03e946a4300b8e45784c2947ba1726e
cswika <csjohniwk@gmail.com>
2025-05-05 08:03:13 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -2,10 +2,6 @@ const items = [
 
     
     {
-        img: '/bukti/img/2025-04-28N.webp',
-        title: 'Gates of Olympus 1000',
-        table: [['Tanggal', '28-04-2025'], ['Modal', 'Rp 50.000'], ['WD', 'Rp 3.000.000']]
-    },{
         img: '/bukti/img/2025-05-01N.webp',
         title: 'Gates of Olympus 1000',
         table: [['Tanggal', '01-05-2025'], ['Modal', 'Rp 550.000'], ['WD', 'Rp 13.045.000']]
@@ -13,6 +9,10 @@ const items = [
         img: '/bukti/img/2025-04-30N.webp',
         title: 'Gates of Olympus 1000',
         table: [['Tanggal', '30-04-2025'], ['Modal', 'Rp 300.000'], ['WD', 'Rp 12.000.000']]
+    },{
+        img: '/bukti/img/2025-04-28N.webp',
+        title: 'Gates of Olympus 1000',
+        table: [['Tanggal', '28-04-2025'], ['Modal', 'Rp 50.000'], ['WD', 'Rp 3.000.000']]
     },{
         img: '/bukti/img/2025-04-23N.webp',
         title: 'Starlight Princess',
