aea30d4b4a2dc4c0cf7203d299d54d0092f67f5a
cswika <csjohniwk@gmail.com>
2026-03-29 13:20:24 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -3,6 +3,11 @@ const items = [
     
     
     {
+        img: '/bukti/img/2026-16-03.1.webp',
+        title: 'Gates of Olympus 1000',
+        table: [['Tanggal', '16-03-2026'], ['Modal', 'Rp 700.000'], ['WD', 'Rp 14.600.000']]
+    },
+    {
         img: '/bukti/img/2026-16-03.webp',
         title: 'Gates of Olympus 1000',
         table: [['Tanggal', '16-03-2026'], ['Modal', 'Rp 1.200.000'], ['WD', 'Rp 28.700.000']]
