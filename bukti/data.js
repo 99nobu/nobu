b3e376368cb3742a16e1c86e9d0be0318312c82f
88romaitaly <88romaitaly@gmail.com>
2026-07-27 05:04:11 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -2,6 +2,16 @@ const items = [
 
     
     {
+        img: '/bukti/img/26.07.2026.webp',
+        title: 'Gates of Olympus 1000',
+        table: [['Tanggal', '26-07-2026'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 10.000.000']]
+    },
+    {
+        img: '/bukti/img/26-07-2026.webp',
+        title: 'Gates of Olympus Super Scatter',
+        table: [['Tanggal', '26-07-2026'], ['Modal', 'Rp 135.000'], ['WD', 'Rp 11.000.000']]
+    },
+    {
         img: '/bukti/img/25-07-2026.webp',
         title: 'Starlight Princess 1000',
         table: [['Tanggal', '25-07-2026'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 15.000.000']]
