822134ace4f78c5767e2f7aa8b512be1d8c3b302
88romaitaly <88romaitaly@gmail.com>
2026-08-05 05:00:38 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -2,6 +2,21 @@ const items = [
 
     
     {
+        img: '/bukti/img/05.08.2026.webp',
+        title: 'Mahjong Wins 2',
+        table: [['Tanggal', '05-08-2026'], ['Modal', 'Rp 230.000'], ['WD', 'Rp 13.000.000']]
+    },
+    {
+        img: '/bukti/img/05-08-2026.webp',
+        title: 'Jade Legends',
+        table: [['Tanggal', '05-08-2026'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 11.500.000']]
+    },
+    {
+        img: '/bukti/img/4-8-2026.webp',
+        title: 'Sweet Bonanza 1000 Dice',
+        table: [['Tanggal', '04-08-2026'], ['Modal', 'Rp 300.000'], ['WD', 'Rp 27.544.000']]
+    },
+    {
         img: '/bukti/img/04-08-2026.webp',
         title: 'Gates of Olympus Super Scatter',
         table: [['Tanggal', '04-08-2026'], ['Modal', 'Rp 98.500'], ['WD', 'Rp 10.518.000']]
