1474ddaa8a1977de773972dc2419b28055cc7db9
88romaitaly <88romaitaly@gmail.com>
2026-08-30 05:42:55 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -3,6 +3,11 @@ const items = [
     
     
     {
+        img: '/bukti/img/30-08-2026.webp',
+        title: 'Mahjong Wins 2',
+        table: [['Tanggal', '30-08-2026'], ['Modal', 'Rp 700.000'], ['WD', 'Rp 20.000.000']]
+    },
+    {
         img: '/bukti/img/28--08-2026.webp',
         title: 'Sugar Rush',
         table: [['Tanggal', '28-08-2026'], ['Modal', 'Rp 100.000'], ['WD', 'Rp 10.000.000']]
