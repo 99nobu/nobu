c6494d8b684dc951cce2de16903f8b8b484df710
88romaitaly <88romaitaly@gmail.com>
2026-09-11 04:11:52 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -3,6 +3,11 @@ const items = [
     
     
     {
+        img: '/bukti/img/10.09.20261.webp',
+        title: 'Mahjong Wins 2',
+        table: [['Tanggal', '10-09-2026'], ['Modal', 'Rp 180.000'], ['WD', 'Rp 16.100.000']]
+    },
+    {
         img: '/bukti/img/10.09.2026.webp',
         title: 'Starlight Princess Super Scatter',
         table: [['Tanggal', '10-09-2026'], ['Modal', 'Rp 500.000'], ['WD', 'Rp 25.000.000']]
