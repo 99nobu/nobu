2889b8171c9da5cab422b8e1285840b137984453
88romaitaly <88romaitaly@gmail.com>
2026-09-16 05:34:47 +0700

    Update data.js

--- bukti/data.js
+++ bukti/data.js
@@ -3,6 +3,11 @@ const items = [
     
     
     {
+        img: '/bukti/img/15-9-2026.webp',
+        title: 'Helios - Triple Sun',
+        table: [['Tanggal', '15-09-2026'], ['Modal', 'Rp 500.000'], ['WD', 'Rp 29.700.000']]
+    },
+    {
         img: '/bukti/img/15-09-20263.webp',
         title: 'Gates of Olympus 1000',
         table: [['Tanggal', '15-09-2026'], ['Modal', 'Rp 400.000'], ['WD', 'Rp 24.515.000']]
